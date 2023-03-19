1aa8538a0d767afaa8b4754361680576e960f062
333Nikita333 <nikitosfox333@gmail.com>
2023-03-19 20:32:35 +0200

    pasted code from Homework-04

---
+++ src/components/Filter/Filter.js
@@ -0,0 +1,27 @@
+import PropTypes from 'prop-types';
+import { LabelFilter, InputFilter } from './Filter.styled';
+
+const Filter = ({ setFilter, filter }) => {
+  function filteredContact(e) {
+    setFilter(e.target.value);
+  }
+
+  return (
+    <LabelFilter>
+      <span>Find contacts by name</span>
+      <InputFilter
+        type="text"
+        name="filter"
+        value={filter}
+        onChange={filteredContact}
+      />
+    </LabelFilter>
+  );
+};
+
+Filter.propTypes = {
+  setFilter: PropTypes.func.isRequired,
+  filter: PropTypes.string,
+};
+
+export default Filter;
